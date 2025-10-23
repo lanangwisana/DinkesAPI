e70a946b91512be9fc64f933af25f2a409f1d86e
lanangwisana <lanangwisana@gmail.com>
2025-10-23 15:09:07 +0700

    Buat model untuk JumlahTenagaMedis

---
+++ Models/JumlahTenagaMedis.cs
@@ -0,0 +1,11 @@
+namespace DinkesAPI.Models {
+    public class JumlahTenagaMedis {
+        public int ID_Jumlahtm { get; set; }
+        public int dokter { get; set; }
+        public int perawat { get; set; }
+        public int bidan { get; set; }
+
+        public int ID_faskes { get; set; }
+        public Faskes Faskes { get; set; }
+    }
+}
