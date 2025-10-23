2253098b5b179285f3ecc8b5230941f4cde45e12
lanangwisana <lanangwisana@gmail.com>
2025-10-23 15:52:50 +0700

    update code DinkesContext.cs

--- Data/DinkesContext.cs
+++ Data/DinkesContext.cs
@@ -1,19 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using DinkesAPI.Models;
 
-namespace DinkesAPI.Data {
-    public class DinkesContext : DbContext {
+namespace DinkesAPI.Data
+{
+    public class DinkesContext : DbContext
+    {
         public DinkesContext(DbContextOptions<DinkesContext> options) : base(options) {}
 
         public DbSet<Faskes> Faskes { get; set; }
         public DbSet<JumlahTenagaMedis> JumlahTenagaMedis { get; set; }
-    }
-}
 
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-    modelBuilder.Entity<JumlahTenagaMedis>()
-        .HasOne(jtm => jtm.Faskes)
-        .WithOne(f => f.JumlahTenagaMedis)
-        .HasForeignKey<JumlahTenagaMedis>(jtm => jtm.ID_faskes);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JumlahTenagaMedis>()
+                .HasOne(jtm => jtm.Faskes)
+                .WithMany(f => f.JumlahTenagaMedis)
+                .HasForeignKey(jtm => jtm.ID_faskes);
+        }
+    }
 }
