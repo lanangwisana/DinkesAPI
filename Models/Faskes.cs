8b32a2522171a30ba5d604d920a6501c1da72bc5
lanangwisana <lanangwisana@gmail.com>
2025-10-23 15:08:08 +0700

    Buat model faskes

---
+++ Models/Faskes.cs
@@ -0,0 +1,10 @@
+namespace DinkesAPI.Models {
+    public class Faskes {
+        public int ID_faskes { get; set; }
+        public string nama_faskes { get; set; }
+        public string jenis_faskes { get; set; }
+        public string alamat_faskes { get; set; }
+
+        public JumlahTenagaMedis JumlahTenagaMedis { get; set; }
+    }
+}
